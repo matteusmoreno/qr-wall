17ac30f72ed36e27aa3f1bc79b8b6dac12a619b0
Matteus Moreno <matteus_moreno@live.com>
2024-06-20 15:31:31 -0300

    commit inicial

---
+++ src/main/java/br/com/matteusmoreno/qrwall/QrwallApplication.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.qrwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrwallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrwallApplication.class, args);
+	}
+
+}
